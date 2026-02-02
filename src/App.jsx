35a9a1e57694347c987704bdca8087b3002600cd
chanti <nirajvarma1928@gmail.com>
2026-02-02 19:51:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [text, setText] = useState("")
+
+  return (
+    <>
+      <input type="text"
+      value={text}
+      onChange={(e)=>setText(e.target.value)}
+      />
+      <div>{text}</div>
+    </>
+  )
+}
+
+export default App
